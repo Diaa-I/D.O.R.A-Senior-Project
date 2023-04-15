51025099f66e388cd9e49bad8487ca3d841e4574
Anas302 <anas302.kh@gmail.com>
2023-04-15 20:24:45 +0400

    Add normalize_coordinates() static method

--- AI/controller/dataManager.py
+++ AI/controller/dataManager.py
@@ -16,7 +16,7 @@ class DataManager(object):
         self.projectName = projectName # 'myProject'
         self.labelsIndex = {i: self.labelsArray[i] for i in range(len(self.labelsArray))} # {0: 'cat', 1: 'dog', 2: 'horse', ...}
         self.labelsToIndex = {self.labelsIndex[i]: i for i in range(len(self.labelsIndex))} # {'cat': 0, 'dog': 1, 'horse': 2, ...}
-
+    
     def createYaml(self, at):
         # Set the paths to the YAML file
         file_path = os.path.join(at, f'{self.projectName}.yaml')
@@ -65,31 +65,46 @@ class DataManager(object):
 
     def storeAsYOLOtxt(self, annObjsArray, at):
         # the annObjArray must be an array in the form of: 
-        #    [[frameFileName, objLabel, x_center, y_center, width, height], [frameFileName, objLabel, x_center, y_center, width, height], ...]
-        # e.g. ['12.JPEG', 'cat', 25.666, 355, 120, 560]
-        # x_center, y_center, width and height must all be normalized relative to the image dimensions.
-        # objLabel is given as a name (e.g. cat) but converted using the labelIndex to a number
+        #    [[frameFileName, img_width, img_height, objLabel, x_center, y_center, width, height], ...]
+        # e.g. ['12.JPEG', 1200, 800, 'cat', 25.666, 355, 120, 560]
+        # CHANGE IT SO THAT AN ANNOTATION FILE CAN HAVE MORE THAN 1 ANNOTATION
         for annObj in annObjsArray:
+            # get image width and height
+            full_path = os.path.join("AI", "sandbox", annObj[0]) # CHANGE 'AI\sandbox' TO 'train_data\images\train'
+            img_height, img_width, _ = cv2.imread(full_path).shape
+
             # change file name to be from .JPG to .TXT
             filename_noExt = annObj[0].split('.')[0]
             path = os.path.join(at, filename_noExt + '.txt')
+
             # create the file, dump all the data into it and close it
             with open(path, 'w') as annFile:
-                objLabel = self.labelsToIndex[annObj[1]]
-                x_center, y_center, width, height = annObj[2], annObj[3], annObj[4], annObj[5]
-                annFile.write(f"{objLabel} {x_center} {y_center} {width} {height}")
+                label = self.labelsToIndex[annObj[1]]
+                x_center, y_center, width, height = self.normalize_coordinates(annObj[2], annObj[3], annObj[4], annObj[5], img_width=img_width, img_height=img_height)
+                annFile.write(f"{label} {x_center} {y_center} {width} {height}")
                 annFile.close()
+    
+    @staticmethod
+    def normalize_coordinates(x, y, width, height, img_width, img_height):
+        # Calculate the normalized values
+        x_norm = x / img_width
+        y_norm = y / img_height
+        w_norm = width / img_width
+        h_norm = height / img_height
+        return x_norm, y_norm, w_norm, h_norm
 
 
-# in use
-# DM = DataManager(['cat', 'dog', 'horse'], 'projName')
-# print("DataManger: initialized")
 
-# DM.createYaml(at="AI\sandbox")
-# print("DataManger: YAML created")
+DM = DataManager(['cat', 'dog', 'horse'], 'projName')
+print("DataManger: initialized")
 
-# DM.extractFrames(videoFile=r"AI\sandbox\sample.mp4", outputDir=r"AI\sandbox")
-# print("DataManger: frames extracted")
+DM.createYaml(at="AI\sandbox")
+print("DataManger: YAML created")
 
-# DM.storeAsYOLOtxt([['0_projName.jpg', 'cat', 25.666, 355, 120, 560]], at=r"AI\sandbox")
-# print("DataManager: .txt annotation files created")
+DM.extractFrames(videoFile=r"AI\sandbox\sample.mp4", outputDir=r"AI\sandbox")
+print("DataManger: frames extracted")
+
+DM.storeAsYOLOtxt([['0_projName.jpg', 'cat', 25.666, 355, 120, 560], 
+                   ['1_projName.jpg', 'dog', 16.566, 50, 40, 1000]], 
+                   at=r"AI\sandbox")
+print("DataManager: .txt annotation files created")
