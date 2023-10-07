c761ff29c370b3599cb1d04d02aced2398e1299c
Seba Al Mokdad <sebamekdad002@gmail.com>
2023-10-07 04:07:15 +0400

    Add heading component

---
+++ frontend/src/Components/Heading.js
@@ -0,0 +1,15 @@
+import './Heading.css'
+function Heading (){
+    return(
+    <>
+        <div class="heading">
+        <input class="form-control me-2 mb-4 custom" type="search" placeholder="Search" aria-label="Search" />
+        <button class="add-project-button" data-bs-toggle = "modal" data-bs-target = "#project-modal">
+        <span class="plus-sign">+ </span> Add New Project</button>
+        </div>
+    </>
+    
+)
+}
+
+export default Heading;
