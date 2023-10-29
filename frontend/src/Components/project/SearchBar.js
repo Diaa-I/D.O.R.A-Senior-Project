ecf84db50a3306bb312a0449c8845f2c9f5b0c73
Seba Al Mokdad <sebamekdad002@gmail.com>
2023-10-29 10:41:51 +0400

    Add searchBar component

---
+++ frontend/src/Components/project/SearchBar.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function  SearchBar(){
+    
+    return(
+    <input class="form-control me-2 mb-4 custom" type="search" placeholder="Search" aria-label="Search" />
+    );
+}
+
+export default SearchBar;
