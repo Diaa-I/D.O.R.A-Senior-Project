77285eda4b12e098521c57a790574ad3f03c3f04
Anas302 <anas302.kh@gmail.com>
2023-11-01 21:23:48 +0400

    Add function for testing make_inference() and apply it to images

--- AI/exp.py
+++ AI/exp.py
@@ -1,6 +1,35 @@
 import controller as con
-import os
 
-pm = con.projectManager.ProjectManager(['plastic', 'metal', 'paper'], project_name="trash_detection")
+image_path = r'C:\Users\anas3\Desktop\metal1.jpg'
+output_path = r'C:\Users\anas3\Desktop\metal1_out.jpg'
 
-pm.create_annotations_txt("frame_00423", 400, 600, [{'label': 'plastic', 'x_center':80, 'y_center': 234, 'width': 30, 'height':80}, {'label': 'plastic', 'x_center':80, 'y_center': 234, 'width': 30, 'height':80}], "./sandbox")
+det = con.modelController.ModelController.make_inference(img=image_path,
+                                                    normalization_dims=(512, 384),
+                                                    yaml_filepath=r"C:\Users\anas3\Desktop\trash_detection.yaml",
+                                                    model_filepath=r"C:\Users\anas3\Desktop\best.pt",
+                                                    conf_threshold=0.1)
+
+print(det)
+
+import cv2
+import numpy as np
+
+def draw_bounding_box(image_path, points, output_path):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Convert points to NumPy array
+    points = np.array(points, dtype=int)
+
+    # Draw the bounding box
+    cv2.polylines(image, [points], isClosed=True, color=(0, 255, 0), thickness=2)
+
+    # Save the image with the bounding box
+    cv2.imwrite(output_path, image)
+
+# Example usage:
+loc = det[0]['location']
+x_min, y_min, x_max, y_max = loc[0], loc[1], loc[2], loc[3]
+
+points = [(x_min, y_min), (x_max, y_min), (x_max, y_max), (x_min, y_max)]  # Replace with your own points
+draw_bounding_box(image_path, points, output_path)
