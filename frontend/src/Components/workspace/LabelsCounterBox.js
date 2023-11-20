7fa75909dad91b5f94945c2d37c17aa7925df817
Seba Al Mokdad <sebamekdad002@gmail.com>
2023-11-20 21:38:42 +0400

    Style Component

--- frontend/src/Components/workspace/LabelsCounterBox.js
+++ frontend/src/Components/workspace/LabelsCounterBox.js
@@ -1,17 +1,23 @@
+import React from 'react';
 
-export default function LabelsCounterBox(props){
-console.log("HELLELEE")
-console.log(props.labelsCounter)
-const labelsObj = [] 
-for(let label in props.labelsCounter){
-    labelsObj.push(<li>{label}{props.labelsCounter[label]}</li>)
-}
-labelsObj.map((element)=>{console.log(element.props.children)})
-return (
-    <div className="a">
-    <ul>
-    {labelsObj.map((element)=>{return element.props.children})}
-    </ul>
-    </div>
-);
+export default function LabelsCounterBox(props) {
+    const { labelsCounter } = props;
+    return (
+        <div className="card" style={{ backgroundColor: '#737373', color: '#ffffff' }}>
+            <div className='card-header bg-primary text-white'>
+            <h6 className="card-title">Label Counts</h6>
+
+            </div>
+            <div className="card-body">
+                <ul className="list-group">
+                    {Object.entries(labelsCounter).map(([label, count]) => (
+                        <li key={label} className="list-group-item d-flex justify-content-between align-items-center" style={{ backgroundColor: '#555555', color: '#ffffff' }}>
+                            {label}
+                            <span className="badge bg-primary">{count}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
